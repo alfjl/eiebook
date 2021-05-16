14104ffa5269e1736927b9b3dbc55453c0d33013
alfjl <77638956+alfjl@users.noreply.github.com>
2021-05-16 20:22:55 +0200

    First multi-line assembler code

--- main_assembler.s
+++ main_assembler.s
@@ -23,5 +23,25 @@
 
 main
 
+move_eg   MOV       r0, #10             ; r0 = 10
+          MOV       r1, r0              ; r1 = r0
+          MOV       r0, r1, LSL #4      ; r0 = r1 * 16
+          MOV       r0, r1, ASR #1      ; r0 = signed(r1 / 2)
+
+move_eg2  MOV       r2, #8              ; r2 = 8
+          MOV       r0, r1, LSL r2      ; r0 = r1 x 2^r2
+          MOV       r0, r1, ASR r2      ; r0 = signed(r1 / 2^r2)
+
+move_eg3  MOV       r0, #0              ; r0 = 0
+          MOV       r1, #0              ; r1 = 0
+          MOV       r2, r1              ; r2 = r1
+          MOVS      r3, #0              ; r3 = 0, update APSR
+          ADD       r0, r0, #256        ; r0 = r0 + 256
+          ADD       r1, r1, r0, LSL #2  ; r1 = r1 + (4 x r0)
+          MOV       r2, r0, LSR #6      ; r2 = r0 / 64
+          SUBS      r3, r1, r0          ; r3 = r1 - r0, update APSR
+          IT        EQ                  ; Get ready for conditional 'EQ'(=equal)
+          MOVEQ     r3, #1024           ; if {Z}, r3 = 1024 (only executed, if result of previous instruction left Zero-flag as '1'. Not in this case!)
+          RSBS      r3, r1, r0, LSL #4  ; r3 = (16 x r0) - r1) ('RSBS' =reverse subtraction (operand2 - operand1))
 
 	END
